e2725583c76d57e5417513e51ccbf2c54dbcf913
zhifeng ou <zhifengou@hotmail.com>
2018-11-17 19:25:27 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
-import { FordQuoteComponent } from './FordQuoteComponent'
-import MouseComponent from '../MouseComponent'
+import FordQuoteComponent from './FordQuoteComponent'
+import MouseComponent from './MouseComponent'
 
 class App extends Component {
   render() {
     // your code in the return statement below!
+    
     return (
       <div className="App">
         <CatComponent />
