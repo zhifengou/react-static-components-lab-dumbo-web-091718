136f13eaefaa7329fcfdd4e3c95a560018ac7f9b
zhifeng ou <zhifengou@hotmail.com>
2018-11-20 02:07:17 +0000

    automatically backed up by learn

--- src/hoverEffect.js
+++ src/hoverEffect.js
@@ -62,7 +62,7 @@ const ctx = canvas.getContext('2d');
 const ripples = [];
 
 
-
+// eslint-disable-next-line
 const rippleStartStatus = 'start';
 
 const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
